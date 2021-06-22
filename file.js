d44677551206e9c0d14509352cd90993f77b33f7
Leonardo Dias <leonardodias@ucl.br>
2021-06-22 12:45:23 -0300

    Add events to portfolio about and contact links

--- file.js
+++ file.js
@@ -3,6 +3,9 @@ const xBtn = document.getElementById('x_btn');
 const mobMenu = document.getElementById('mobile_menu');
 const headline = document.getElementById('headline');
 const navbar = document.getElementById('nav');
+const portfolio = document.getElementById('mobile_prt');
+const about = document.getElementById('mobile_abt');
+const contact = document.getElementById('mobile_cnt');
 
 hamburguer.addEventListener('click', () => {
   mobMenu.style.display = 'flex';
@@ -11,6 +14,24 @@ hamburguer.addEventListener('click', () => {
 });
 
 xBtn.addEventListener('click', () => {
+  mobMenu.style.display = 'none';
+  headline.style = '';
+  navbar.style = '';
+});
+
+portfolio.addEventListener('click', () => {
+  mobMenu.style.display = 'none';
+  headline.style = '';
+  navbar.style = '';
+});
+
+about.addEventListener('click', () => {
+  mobMenu.style.display = 'none';
+  headline.style = '';
+  navbar.style = '';
+});
+
+contact.addEventListener('click', () => {
   mobMenu.style.display = 'none';
   headline.style = '';
   navbar.style = '';
